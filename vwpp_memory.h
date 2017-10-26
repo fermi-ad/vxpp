daa5150ad5bbcb05d70055a720d765fd2d7bdda0
Rich Neswold <neswold@fnal.gov>
2017-10-26 15:43:40 -0500

    Improve the API for @vwpp::VME::A16@.

--- vwpp_memory.h
+++ vwpp_memory.h
@@ -15,18 +15,18 @@ namespace vwpp {
     //
     //  * Serialized access to A16 space
 
-    namespace vme {
+    namespace VME {
 
 	char* calcA16BaseAddr(uint32_t);
 
 	// This class controls access to VME A16 space. It requires a
 	// mutex to be held during access to the hardware and it
-	// range-checks all accesses so they don't exceed the
-	// hardware's span.
+	// range-checks all accesses -- at compile time -- so they
+	// don't exceed the hardware's span.
 
 	template <typename LockType, size_t size>
 	class A16 {
-	    char* const baseAddr;
+	    char volatile* const baseAddr;
 
 	    // This template expands to a type that defines 'type'
 	    // only if an offset into a VME::A16 register bank is
@@ -40,40 +40,29 @@ namespace vwpp {
 	    template <typename RT, size_t offset>
 	    struct Accessible<RT, offset, false> {};
 
+	    typedef typename DetermineLock<LockType>::type Lock;
+
 	 public:
-	    explicit A16(uint32_t const a16_offset) :
+	    explicit A16(uint16_t const a16_offset) :
 		baseAddr(calcA16BaseAddr(a16_offset)) {}
 
 	    void* getBaseAddr() const { return baseAddr; }
 
-	    typedef typename DetermineLock<LockType>::type Lock;
-
 	    template <typename RT, size_t offset>
-	    struct Reg {
+	    RT get(Lock const&) const
+	    {
 		typedef typename Accessible<RT, offset>::allowed type;
 
-		RT get(Lock const&) const
-		{
-		    return *reinterpret_cast<RT volatile*>(baseAddr + offset);
-		}
-
-		void set(Lock const&, RT const& v)
-		{
-		    *reinterpret_cast<RT volatile*>(baseAddr + offset) = v;
-		}
-	    };
-
-	    template <size_t offset>
-	    struct Reg<void, offset> {
-	    };
+		return *reinterpret_cast<RT volatile*>(baseAddr + offset);
+	    }
 
 	    template <typename RT, size_t offset>
-	    struct Reg<RT*, offset> {
-	    };
+	    void set(Lock const&, RT const v)
+	    {
+		typedef typename Accessible<RT, offset>::allowed type;
 
-	    template <typename RT, size_t offset>
-	    struct Reg<RT&, offset> {
-	    };
+		*reinterpret_cast<RT volatile*>(baseAddr + offset) = v;
+	    }
 	};
     };
 };
