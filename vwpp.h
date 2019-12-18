7f850157f1e91034b14b4a7df48e8e39516c8a9d
Rich Neswold <neswold@fnal.gov>
2019-12-18 15:42:34 -0600

    Add "memory" clobber attribute.
    The three macros used for memory synchronization (`MEMORY_SYNC`,
    `INSTRUCTION_SYNC`, and `ALL_SYNC`) still suffered from compiler
    optimization issues. These new versions add an attribute that
    tells the compiler to not optimize global memory access across
    the `asm` statements.

--- vwpp.h
+++ vwpp.h
@@ -41,9 +41,9 @@
 // and instruction fetches until all previous activity is complete.
 
 #if defined(PPC603) || defined(PPC604) || defined(PPC750) || defined(PPC7400)
-#define	MEMORY_SYNC		__asm__ volatile (" eieio")
-#define	INSTRUCTION_SYNC	__asm__ volatile (" isync")
-#define	ALL_SYNC		__asm__ volatile (" sync")
+#define	MEMORY_SYNC		asm volatile (" eieio" ::: "memory")
+#define	INSTRUCTION_SYNC	asm volatile (" isync" ::: "memory")
+#define	ALL_SYNC		asm volatile (" sync" ::: "memory")
 #endif
 
 // These forward-declared structures and functions are found in
